0d0d4f61289e6ebdf64bf4ccaed418ea7b054323
Kriegslustig <github@ls7.ch>
2016-02-10 18:37:22 +0100

    Add elm:make as a dep

--- package.js
+++ package.js
@@ -2,12 +2,12 @@ Package.describe(
   { name: 'elm:mongo'
   , summary: 'Mongo bindings for Elm'
   , version: '0.0.1'
-  //, git: ''
+  , git: 'https://github.com/Kriegslustig/meteor-elm-mongo.git'
   }
 )
 
 Package.onUse(function (api) {
-  api.use(['ecmascript@0.1.6', 'mongo'])
+  api.use(['ecmascript@0.1.6', 'elm:make', 'mongo'])
   api.addFiles(
     [ 'Mongo.elm'
     ]
